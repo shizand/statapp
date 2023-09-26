ac78320440750ef9d649ff96832b556d11bc45c0
MisterMLiL <eugenelazurenko@gmail.com>
2023-09-26 15:56:49 +0300

    Исправил main_window.py

--- statapp/main_window.py
+++ statapp/main_window.py
@@ -6,8 +6,6 @@ from statapp.ui.ui_main_window import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
-    about_window = None
-
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()

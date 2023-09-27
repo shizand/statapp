12eb070591098884b1d3d43edfa0b6f667821ce0
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-09-27 20:10:22 +0300

    fix: обновляет окно "О программе"

--- statapp/about_window.py
+++ statapp/about_window.py
@@ -1,12 +1,18 @@
+import sys
 from PySide2.QtGui import QMovie
 from PySide2.QtWidgets import QMainWindow
 
 from statapp.ui.ui_about_window import Ui_AboutWindow
 from statapp.utils import resource_path
 
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
 
 class AboutWindow(QMainWindow):
-    pixmap = None
+    movie = None
 
     def __init__(self):
         super().__init__()
@@ -14,10 +20,11 @@ class AboutWindow(QMainWindow):
         self.ui.setupUi(self)
 
         image_path = resource_path('images/sticker.gif')
-
         movie = QMovie(image_path)
         self.ui.labelgif.setMovie(movie)
         movie.start()
-
-        self.pixmap = movie
+        self.movie = movie
         self.setFixedSize(self.size())
+
+        version = importlib_metadata.version(__package__ or __name__)
+        self.ui.versionLabel.setText(f"Версия: {version}")

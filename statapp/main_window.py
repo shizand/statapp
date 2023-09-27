7a2762bd665f4cab40d2e2322925e261523e3c24
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-09-27 09:41:50 +0300

    fix: добавляет поддержку Windows 7

--- statapp/main_window.py
+++ statapp/main_window.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import QMainWindow
 from statapp.about_window import AboutWindow
 from statapp.ui.ui_main_window import Ui_MainWindow
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()

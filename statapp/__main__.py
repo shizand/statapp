2b061bed2f6343fc2feb87472afc4c9a051b30a9
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-09-28 15:43:01 +0300

    fix: исправлена локализация стандартных кнопок

--- statapp/__main__.py
+++ statapp/__main__.py
@@ -1,4 +1,6 @@
 import sys
+
+from PySide2 import QtCore
 from PySide2.QtWidgets import QApplication
 
 from statapp.main_window import MainWindow
@@ -6,6 +8,13 @@ from statapp.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
+
+    translator = QtCore.QTranslator(app)
+    locale = QtCore.QLocale.system().name()
+    path = QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.TranslationsPath)
+    translator.load('qt_%s' % locale, path)
+    app.installTranslator(translator)
+
     window = MainWindow()
     window.show()
     return app.exec_()

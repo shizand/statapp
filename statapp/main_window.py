b73a3f5d3ba5707f0bdb816452ad7f59c0da8290
MisterMLiL <99662459+MisterMLiL@users.noreply.github.com>
2023-09-28 14:57:41 +0300

    feat: добавлено чтение и запись исходных данных в файлы .txt, .csv (#24)
    Closes #22
    
    ---------
    
    Co-authored-by: Maxim Slipenko <no-reply@maxim.slipenko.com>

--- statapp/main_window.py
+++ statapp/main_window.py
@@ -1,22 +1,66 @@
 import numpy as np
-from PySide2.QtCore import Slot
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtCore import Slot, QLocale
+from PySide2.QtWidgets import QMainWindow, QMessageBox, QApplication
 
 from statapp.generate_factor_window import GenerateFactorWindow, INDIRECT_LINK
 from statapp.models.data_model import DataModel
 from statapp.generate_window import GenerateWindow
 from statapp.about_window import AboutWindow
+from statapp.models.fileslc_model import FileSLCModel
 from statapp.ui.ui_main_window import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.isDataChanged = False
         self.model = DataModel()
+        self.fileModel = FileSLCModel()
         self.ui.tableView.setModel(self.model)
+
+    @Slot()
+    def on_openfileaction_triggered(self):
+        current_data = self.model.getData()
+        if current_data.size > 1:
+            file = ''
+            if self.fileModel.file_name:
+                file = '\nФайл сохранения:' + self.fileModel.file_name
+
+            msgBox = self.createMessageBox \
+                ('Сохранение данных',
+                 "Сохранить данные?" + file,
+                 QMessageBox.Question,
+                 QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel,
+                 QMessageBox.Cancel)
+
+            reply = msgBox.exec_()
+            if reply == QMessageBox.StandardButton.Yes:
+                self.fileModel.saveFile(self.model.getData())
+            elif reply == QMessageBox.StandardButton.Cancel:
+                return
+            else:
+                data = self.fileModel.loadFile()
+                if data is not None:
+                    self.model.updateAllData(data)
+                    self.isDataChanged = True
+        else:
+            data = self.fileModel.loadFile()
+            if data is not None:
+                self.model.updateAllData(data)
+                self.isDataChanged = True
+
+    @Slot()
+    def on_savefileaction_triggered(self):
+        self.isDataChanged = not self.fileModel.saveFile(self.model.getData())
+
+    @Slot()
+    def on_closefileaction_triggered(self):
+        self.fileModel.closeFile()
+        self.isDataChanged = False
 
     @Slot()
     def on_generateYaction_triggered(self):
@@ -24,6 +68,7 @@ class MainWindow(QMainWindow):
         if gw.exec():
             y = np.random.normal(gw.mat, gw.deviation, size=(gw.count, 1))
             self.model.updateAllData(y)
+            self.isDataChanged = True
 
     @Slot()
     def on_generateXaction_triggered(self):
@@ -58,9 +103,45 @@ class MainWindow(QMainWindow):
             data = np.concatenate((data, x_arr), axis=1)
             # data = np.concatenate((data, dd), axis=1)
             self.model.updateAllData(data)
+            self.isDataChanged = True
 
     @Slot()
     def on_aboutmenuaction_triggered(self):
         global about_window
         about_window = AboutWindow()
         about_window.show()
+
+    def createMessageBox(self, title, text, icon, buttons, defaultButton):
+        msgBox = QMessageBox()
+
+        msgBox.setIcon(icon)
+        msgBox.setWindowTitle(title)
+        msgBox.setText(text)
+        msgBox.setStandardButtons(buttons)
+        msgBox.setDefaultButton(defaultButton)
+
+        return msgBox
+
+    def closeEvent(self, event):
+        if self.isDataChanged:
+            file = ''
+            if self.fileModel.file_name:
+                file = '\nФайл сохранения:' + self.fileModel.file_name
+
+            msgBox = self.createMessageBox \
+                ('Завершение работы',
+                 "Сохранить данные?" + file,
+                 QMessageBox.Question,
+                 QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel,
+                 QMessageBox.Cancel)
+
+            reply = msgBox.exec_()
+            if reply == QMessageBox.StandardButton.Yes:
+                self.fileModel.saveFile(self.model.getData())
+                event.accept()
+            elif reply == QMessageBox.StandardButton.No:
+                event.accept()
+            else:
+                event.ignore()
+        else:
+            event.accept()
---
+++ statapp/models/fileslc_model.py
@@ -0,0 +1,33 @@
+import numpy as np
+from PySide2.QtWidgets import QFileDialog, QMessageBox
+
+
+class FileSLCModel:
+    def __init__(self):
+        super().__init__()
+        self.file_name = None
+
+    def saveFile(self, data):
+        if not self.file_name:
+            self.file_name, _ = QFileDialog.getSaveFileName(None, "Сохранить файл", "", "Text Files (*.txt);;CSV Files (*.csv)")
+        if self.file_name:
+            np.savetxt(self.file_name, data, delimiter=",")
+            return True
+        return False
+
+    def loadFile(self):
+        self.file_name, _ = QFileDialog.getOpenFileName(None, "Загрузить файл", "", "Files (*.txt;*.csv)")
+        if not self.file_name:
+            try:
+                content = np.genfromtxt(self.file_name, delimiter=',', invalid_raise=True)
+            except ValueError as e:
+                QMessageBox.warning \
+                    (None,
+                    'Ошибка',
+                    "Ошибка чтения файла!\nФайл нельзя открыть или файл неверного формата")
+                return None
+            return content
+
+    def closeFile(self):
+        self.file_name = None
+        pass

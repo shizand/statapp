88ceabf0199c5073de2b4020f523fcc33ec4b05f
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-10-03 14:47:35 +0300

    chore: добавлены заголовки о лицензии (#60)

--- statapp/ui/ui_variance_analysis_window.py
+++ statapp/ui/ui_variance_analysis_window.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Maxim Slipenko, Eugene Lazurenko.
+#
+# This file is part of Statapp
+# (see https://github.com/shizand/statapp).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 
 ################################################################################
 ## Form generated from reading UI file 'variance_analysis_window.ui'
@@ -39,4 +59,3 @@ class Ui_VarianceAnalysisWindow(object):
     def retranslateUi(self, VarianceAnalysisWindow):
         VarianceAnalysisWindow.setWindowTitle(QCoreApplication.translate("VarianceAnalysisWindow", u"\u0414\u0438\u0441\u043f\u0435\u0440\u0441\u0438\u043e\u043d\u043d\u044b\u0439 \u0430\u043d\u0430\u043b\u0438\u0437", None))
     # retranslateUi
-
